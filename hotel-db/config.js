b33df5539a0888550219e0d4c31ee2c473c3a5fb
Alejandro <soniccrest34@gmail.com>
2019-04-30 11:52:26 -0500

    db confic

--- hotel-db/config.js
+++ hotel-db/config.js
@@ -4,7 +4,7 @@ const config = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'test'
+  database: process.env.DB_NAME || 'hotel'
 }
 
 module.exports = config
